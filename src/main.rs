d82e9c86f4ea5ee3255e1aa6a2af75507aa335a2
Bushsbakedbean <atleecircuitree@gmail.com>
2023-10-12 17:36:35 -0400

    Sql query testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let connection = sqlite::open("test.db").unwrap();
+
+    let query = "
+        INSERT INTO Logs VALUES ('i', 47553, datetime('now'));
+        INSERT INTO Logs VALUES ('o', 47553, datetime('now'));
+    ";
+    connection.execute(query).unwrap(); 
 }
