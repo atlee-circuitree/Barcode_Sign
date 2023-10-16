578f89024db62fda5d174d5d73b0088a7ca624d7
Owen Carey <atleecircuitree@gmail.com>
2023-10-16 16:39:33 -0400

    fix crate name
    stop complaining rust analyzer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+
 fn main() {
     let connection = sqlite::open("test.db").unwrap();
-
     let query = "
         INSERT INTO Logs VALUES ('i', 47553, datetime('now'));
         INSERT INTO Logs VALUES ('o', 47553, datetime('now'));
