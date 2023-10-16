9ae831b6f6e5582b7576b916d6a771adaef2a580
Owen Carey <atleecircuitree@gmail.com>
2023-10-16 17:14:36 -0400

    move to result based program flow instead of unwrap based

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
+/*
+SQL Logic goal
 
-fn main() {
-    let connection = sqlite::open("test.db").unwrap();
+- take a row
+- find the next entry that matches the same student id
+- if action of found entry is i (in) stop there
+- if action is o (out) record the duration and write to another table
+*/
+
+use std::error::Error;
+fn main() -> Result<(), Box<dyn Error>> {
+    let connection = sqlite::open("test.db")?;
     let query = "
         INSERT INTO Logs VALUES ('i', 47553, datetime('now'));
         INSERT INTO Logs VALUES ('o', 47553, datetime('now'));
     ";
-    connection.execute(query).unwrap(); 
+    connection.execute(query)?; 
+    Ok(())
 }
