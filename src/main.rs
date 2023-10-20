37434873ccfd87fc7f9d5bcd6e8e5b9de00f2705
Owen Carey <atleecircuitree@gmail.com>
2023-10-20 11:05:50 -0400

    move main to module

--- src/main.rs
+++ src/main.rs
@@ -6,43 +6,11 @@ SQL Logic goal
 - if action of found entry is i (in) stop there
 - if action is o (out) record the duration and write to another table
 */
-use std::io;
 use std::error::Error;
-use rusqlite::{Connection, Result};
+mod program;
 
 fn main() -> Result<(), Box<dyn Error>> {
-
-    let connection = Connection::open("test.db")?;
-
-    let query_init = "CREATE TABLE IF NOT EXISTS logs (id INT, act CHAR, time TEXT)";
-    connection.execute(query_init, ())?;
-    
-    loop {
-        let input: String = strin();
-        if input == "X" {
-            break
-        };
-
-        let id:String = String::from(&input[1..]);
-        let id_int = match id.parse::<i32>() {
-            Ok(i) => i,
-            _ => 0
-        };
-        
-        let mut act: String = String::from(&input[..1]);
-        if act.len() > 1 {
-            act = "x".to_string();
-        }
-
-        let query: String = format!("INSERT INTO logs VALUES ({id_int}, '{act}', datetime('now'));");
-        connection.execute(&query, ())?;
-    }
+    program::log()?;
     Ok(())
 }
 
-fn strin() -> String {
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer).unwrap();
-    buffer = String::from(buffer.trim());
-    buffer
-}
---
+++ src/program.rs
@@ -0,0 +1,59 @@
+use std::io;
+use std::error::Error;
+use rusqlite::{Connection, Result};
+enum _ProgramState {
+    Logging,
+    Analyzing,
+    Displaying,
+    Exporting,
+    Idle
+}
+
+fn _state_change() {
+    todo!();
+}
+
+pub fn log() -> Result<(), Box<dyn Error>> {
+    let connection = Connection::open("test.db")?;
+
+    let query_init = "CREATE TABLE IF NOT EXISTS logs (id INT, act CHAR, time TEXT)";
+    connection.execute(query_init, ())?;
+    
+    loop {
+        let input: String = strin();
+        if input == "X" {
+            break
+        };
+
+        let id:String = String::from(&input[1..]);
+        let id_int = match id.parse::<i32>() {
+            Ok(i) => i,
+            _ => 0
+        };
+
+        let act: String = String::from(&input[..1]);
+        
+        let query: String = format!("INSERT INTO logs VALUES ({id_int}, '{act}', datetime('now'));");
+        connection.execute(&query, ())?;
+    }
+    Ok(())
+}
+
+pub fn _analyze() {
+    todo!();
+}
+
+pub fn _display() {
+    todo!();
+}
+
+pub fn _export() {
+    todo!();
+}
+
+fn strin() -> String {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    buffer = String::from(buffer.trim());
+    buffer
+}
