061e2aa0faa6278ed43abbeadfa8b429a8e01c3e
Owen Carey <atleecircuitree@gmail.com>
2023-10-21 13:46:24 -0400

    fix find_logout

--- src/program.rs
+++ src/program.rs
@@ -4,16 +4,18 @@ use std::io;
 #[derive(Debug, Clone)]
 pub struct Entry {
     id: i32,
-    act: String,
+    act: i32,
     time: String,
 }
 
 impl Entry {
-    pub fn print(self) {
+    pub fn _print(self) {
         println!("ID {}, ACTION {}, TIME {}", self.id, self.act, self.time)
     }
 
 }
+
+#[derive(Debug, Clone)]
 pub struct Session {
     id: i32,
     in_time: String,
@@ -24,6 +26,9 @@ impl Session {
         println!("ID {}, IN TIME {}, OUT TIME {}", self.id, self.in_time, self.out_time);
     }
 }
+
+//enum for tracking program state. probably wont be using this any time soon
+/*
 enum _ProgramState {
     Logging,
     Analyzing,
@@ -31,15 +36,15 @@ enum _ProgramState {
     Exporting,
     Idle,
 }
-
 fn _state_change() {
     todo!();
 }
+*/
 
 pub fn log() -> Result<(), Box<dyn Error>> {
     let connection = Connection::open("test.db")?;
 
-    let query_init = "CREATE TABLE IF NOT EXISTS logs (id INT, act CHAR, time TEXT)";
+    let query_init = "CREATE TABLE IF NOT EXISTS logs (id INT, act BIT, time TEXT)";
     connection.execute(query_init, ())?;
 
     loop {
@@ -53,19 +58,30 @@ pub fn log() -> Result<(), Box<dyn Error>> {
             Ok(i) => i,
             _ => 0,
         };
+        
 
-        let act: String = String::from(&input[..1]);
+        let act = &input[..1];
 
+        let act = match act {
+            "i" => 1,
+            "o" => 0,
+            _ => 0,
+        };
         let query: String =
-            format!("INSERT INTO logs VALUES ({id_int}, '{act}', datetime('now'));");
+            format!("INSERT INTO logs VALUES ({id_int}, {act}, datetime('now'));");
         connection.execute(&query, ())?;
     }
     Ok(())
 }
 
 pub fn _analyze() -> Result<(), Box<dyn Error>> {
+    
     let conn = Connection::open("test.db")?;
     let mut stmt = conn.prepare("SELECT id, act, time FROM logs")?;
+    /*
+    I have no idea what this code is doing, I adapted it from the documentation
+    in the inevitable case I need to read from the db again, just replace the struct with whatever is suitable
+    */
     let logs_iter = stmt.query_map([], |row| {
         Ok(Entry {
             id: row.get(0)?,
@@ -82,10 +98,13 @@ pub fn _analyze() -> Result<(), Box<dyn Error>> {
     let mut sessions: Vec<Session> = Vec::new();
 
     for i in 0..(entries.len()-1) {
-        match find_logout(&entries, i) {
-            Some(a) => sessions.push(a),
-            None => (),
+        if entries[i].act == 1 {
+            match find_logout(&entries, i) {
+                Some(a) => sessions.push(a),
+                None => (),
+            }
         }
+        
     }
     for i in sessions {
         i.print();
@@ -94,15 +113,18 @@ pub fn _analyze() -> Result<(), Box<dyn Error>> {
 }
 
 
-
+//This will be the function used to search for specific users and stats eventually
 pub fn _display() {
     todo!();
 }
 
+//this might be used to export to some sort of file that can then be used for apis
+//keyword might since idk how async works
 pub fn _export() {
     todo!();
 }
 
+// just a shorthand for taking input. probably will replace with a CLI library later
 fn strin() -> String {
     let mut buffer = String::new();
     io::stdin().read_line(&mut buffer).unwrap();
@@ -110,14 +132,22 @@ fn strin() -> String {
     buffer
 }
 
+//iterates through the list of entries, finding the next one with the same id
+//then makes a struct with the time of that entry if it is a logout, or with the same time if it is a login
+//functionally recreates that monstrosity of a cell equation from the google sheet
 fn find_logout(table: &Vec<Entry>, session_start: usize) -> Option<Session>{
     let start_entry = table[session_start].clone();
     let future_entries = &table[(session_start + 1)..];
+    for entry in future_entries {
+        let entry = entry.clone();
+        entry._print();
+    }
+    println!();
     for e in future_entries {
         let entry = e.clone();
         if entry.id == start_entry.id {
-            match entry.act.as_str() {
-                "i" => {
+            match entry.act{
+                0 => {
                     let s = Session{
                         id: start_entry.id,
                         in_time: start_entry.time.clone(),
@@ -125,7 +155,7 @@ fn find_logout(table: &Vec<Entry>, session_start: usize) -> Option<Session>{
                     };
                     return Some(s)
                 },
-                _ => {
+                1 => {
                     let s = Session{
                         id: start_entry.id,
                         in_time: start_entry.time.clone(),
@@ -133,6 +163,7 @@ fn find_logout(table: &Vec<Entry>, session_start: usize) -> Option<Session>{
                     };
                     return Some(s)
                 }
+                _ => return None,
             }
         }
     }
